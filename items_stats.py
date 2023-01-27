2d14e08ecbc07af0647cbe143af7a26477581b51
Matteo Leturcq--Daligaux <matteo.leturcq--daligaux@etu.minesparis.psl.eu>
2023-01-27 13:07:26 +0100

    Debug Room

--- items_stats.py
+++ items_stats.py
@@ -1,48 +1,80 @@
 import pygame as pg
 import random
 
-opened_texture = pg.transform.rotozoom((pg.image.load("textures/opened_chest.png").convert_alpha()), 0, 0.1)
-closed_texture = pg.transform.rotozoom((pg.image.load("textures/closed_chest.png").convert_alpha()), 0, 0.1)
+S_WIDTH, S_HIGHT = 800, 700
+screen = pg.display.set_mode((S_WIDTH, S_HIGHT))
+opened_texture = pg.transform.rotozoom(
+    (pg.image.load("textures/opened_chest.png").convert_alpha()), 0, 0.1
+)
+closed_texture = pg.transform.rotozoom(
+    (pg.image.load("textures/closed_chest.png").convert_alpha()), 0, 0.1
+)
 
 
 Stats = {"HP_MAX": 100, "ATK": 10, "SPD": 15, "DEF": 0, "RANGE": 50, "FIRE_RATE": 100}
 
-class Item:
 
+class Item:
     def __init__(self, name, effect):
         self.name = name
-        self.texture = pg.image.load(f"textures/{self.name.replace(' ', '_') }.webp").convert_alpha()
+        self.texture = pg.image.load(
+            f"textures/{self.name.replace(' ', '_') }.webp"
+        ).convert_alpha()
         self.effect = effect
-    
+
     def item_get(self):
         for st in Stats.keys():
             st += self.effect[st]
 
 
+brimstone = Item(
+    "Brimstone",
+    {"HP_MAX": 0, "ATK": 5, "SPD": 0, "DEF": 0, "RANGE": 20, "FIRE_RATE": 10},
+)
 
-brimstone = Item("Brimstone", {"HP_MAX": 0, "ATK": 5, "SPD": 0, "DEF": 0, "RANGE": 20, "FIRE_RATE": 10})
+magicmush = Item(
+    "Magic Mushroom",
+    {"HP_MAX": 10, "ATK": 3, "SPD": 5, "DEF": 3, "RANGE": 10, "FIRE_RATE": -10},
+)
 
-magicmush = Item("Magic Mushroom", {"HP_MAX": 10, "ATK": 3, "SPD": 5, "DEF": 3, "RANGE": 10, "FIRE_RATE": -10})
+ipecac = Item(
+    "Ipecac",
+    {"HP_MAX": 0, "ATK": 10, "SPD": 0, "DEF": -3, "RANGE": 20, "FIRE_RATE": 15},
+)
 
-ipecac = Item("Ipecac", {"HP_MAX": 0, "ATK": 10, "SPD": 0, "DEF": -3, "RANGE": 20, "FIRE_RATE": 15})
+plan_c = Item(
+    "Plan C", {"HP_MAX": 0, "ATK": 5, "SPD": 0, "DEF": 0, "RANGE": 20, "FIRE_RATE": 10}
+)
 
-plan_c = Item("Plan C", {"HP_MAX": 0, "ATK": 5, "SPD": 0, "DEF": 0, "RANGE": 20, "FIRE_RATE": 10})
+c_section = Item(
+    "C Section",
+    {"HP_MAX": 0, "ATK": 5, "SPD": 0, "DEF": 0, "RANGE": 5, "FIRE_RATE": 10},
+)
 
-c_section = Item("C Section", {"HP_MAX": 0, "ATK": 5, "SPD": 0, "DEF": 0, "RANGE": 5, "FIRE_RATE": 10})
+bobby = Item(
+    "Brother Bobby",
+    {"HP_MAX": 0, "ATK": 1, "SPD": 0, "DEF": 2, "RANGE": 10, "FIRE_RATE": -10},
+)
 
-bobby = Item("Brother Bobby", {"HP_MAX": 0, "ATK": 1, "SPD": 0, "DEF": 2, "RANGE": 10, "FIRE_RATE": -10})
+maggy = Item(
+    "Sister Maggy",
+    {"HP_MAX": 0, "ATK": 1, "SPD": 0, "DEF": 2, "RANGE": 10, "FIRE_RATE": -10},
+)
 
-maggy = Item("Sister Maggy", {"HP_MAX": 0, "ATK": 1, "SPD": 0, "DEF": 2, "RANGE": 10, "FIRE_RATE": -10})
-
-lilbrim = Item("Lil Brimstone", {"HP_MAX": 0, "ATK": 2, "SPD": 0, "DEF": 0, "RANGE": 10, "FIRE_RATE": 0})
+lilbrim = Item(
+    "Lil Brimstone",
+    {"HP_MAX": 0, "ATK": 2, "SPD": 0, "DEF": 0, "RANGE": 10, "FIRE_RATE": 0},
+)
 
 item_list = [brimstone, magicmush, ipecac, plan_c, c_section, bobby, maggy, lilbrim]
 
-class Chest:
 
-    def __init__(self, item = random.choice(item_list), status = "closed", texture = closed_texture):
-        self.content = item   
-        self.status = status        #Ouvert/fermé
+class Chest:
+    def __init__(
+        self, item=random.choice(item_list), status="closed", texture=closed_texture
+    ):
+        self.content = item
+        self.status = status  # Ouvert/fermé
         self.texture = texture
 
     def open(self):
@@ -50,19 +82,8 @@ class Chest:
         self.texture = opened_texture
 
 
-
-
-
-
-
-
-
-
-
-''' if event.key == pg.K_o:
+""" if event.key == pg.K_o:
         coffre1.texture.fill((255,255,255))
         coffre1.open()
         print(coffre1.content)
-'''
-
-
+"""
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame as pg
 from items_stats import *
 from rooms import Room
-
+from ennemies import *
 
 S_WIDTH, S_HIGHT = 800, 700
 # R_WIDTH, R_HIGHT = 300, 500
@@ -9,40 +9,36 @@ R_COLOR = [122, 52, 24]
 D_w, D_h = 50, 10
 D_COLOR = [196, 102, 65]
 
-class Player():
 
+class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-class Room(pg.Rect):
-
 
 coffre1 = Chest()
+
+
 def main():
     clock = pg.time.Clock()
 
     pg.init()
     screen = pg.display.set_mode((S_WIDTH, S_HIGHT))
 
-    def create_room():
-        room_1 = Room(0, 0, S_WIDTH, S_HIGHT)
-        door_t = Room((S_WIDTH - D_w) / 2, 0, D_w, D_h)
-
-        pg.draw.rect(screen, R_COLOR, room_1, width=5)
-        pg.draw.rect(screen, D_COLOR, door_t, width=D_h // 2)
-
-    player = Player(100,100)
+    player = Player(100, 100)
 
     run = True
     while run:
         clock.tick(50)
         # code à mettre ici pour ce qu'il se passe entre 2 images
-        
+
         screen.fill((133, 80, 64))
-        screen.blit(coffre1.texture,(200,200))
-        pg.draw.circle(screen, (200,0,200), (player.x,player.y), 10)
-        Room.draw_room()
+        screen.blit(coffre1.texture, (200, 200))
+        pg.draw.circle(screen, (200, 0, 200), (player.x, player.y), 10)
+        mob1 = Ennemy(50, 2)
+        room1 = Room([mob1], 1)
+        room1.draw_room(screen)
+
         pg.display.update()
 
         for event in pg.event.get():
@@ -56,16 +52,16 @@ def main():
         keys = pg.key.get_pressed()
 
         if keys[pg.K_DOWN]:
-            player.y += Stats["SPD"]/10
-        
+            player.y += Stats["SPD"] / 10
+
         if keys[pg.K_UP]:
-            player.y -= Stats["SPD"]/10
+            player.y -= Stats["SPD"] / 10
 
         if keys[pg.K_LEFT]:
-            player.x -= Stats["SPD"]/10
-        
+            player.x -= Stats["SPD"] / 10
+
         if keys[pg.K_RIGHT]:
-            player.x += Stats["SPD"]/10
+            player.x += Stats["SPD"] / 10
 
         if keys[pg.K_o]:
             coffre1.open()
