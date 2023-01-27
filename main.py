bf3975e8058a8fcc47e72cc0115cf65d135683cc
Tom Perrin <tom.perrin@etu.minesparis.psl.eu>
2023-01-27 10:25:36 +0100

    added movement and a few stats

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pygame as pg
 
-
 def main():
     clock = pg.time.Clock()
 
     pg.init()
     screen = pg.display.set_mode((600, 500))
 
-    done = False
-    while not done:
+    x,y = 0,0
+    delta = 10
+
+    hp = 100
+    atk = 10
+    df = 0
+
+    run = True
+    while run:
         clock.tick(50)
 
         # code à mettre ici pour ce qu'il se passe entre 2 images
@@ -17,11 +23,23 @@ def main():
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                done = True
+                run = False
 
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_q or event.key == pg.K_ESCAPE:
-                    done = True
+                    run = False
+                
+                if event.key == pg.K_DOWN:
+                    y -= delta
+                
+                if event.key == pg.K_UP:
+                    y += delta
+
+                if event.key == pg.K_LEFT:
+                    x -= delta
+                
+                if event.key == pg.K_RIGHT:
+                    x += delta
     pg.quit()
 
 
