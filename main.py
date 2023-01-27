dfae427ae1e58dce272013c6590eb1ac3b610b9e
Nizar <stephane.abdulnour@etu.minesparis.psl.eu>
2023-01-27 10:54:22 +0100

    Walls and door

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import pygame as pg
 
+S_WIDTH, S_HIGHT = 800, 700
+# R_WIDTH, R_HIGHT = 300, 500
+R_COLOR = [122, 52, 24]
+D_w, D_h = 50, 10
+D_COLOR = [196, 102, 65]
+
+class Room(pg.Rect):
+
+    def __init__(self, l, t, w, h):
+        super().__init__(l, t, w, h)
+
 
 def main():
     clock = pg.time.Clock()
 
     pg.init()
-    screen = pg.display.set_mode((600, 500))
+    screen = pg.display.set_mode((S_WIDTH, S_HIGHT))
+
+    def create_room():
+        room_1 = Room(0, 0, S_WIDTH, S_HIGHT)
+        door_t = Room((S_WIDTH-D_w)/2, 0, D_w, D_h)
+
+        pg.draw.rect(screen, R_COLOR, room_1, width= 5)
+        pg.draw.rect(screen, D_COLOR, door_t, width= D_h//2)
 
     done = False
     while not done:
         clock.tick(50)
 
+        create_room()
         # code à mettre ici pour ce qu'il se passe entre 2 images
 
         pg.display.update()
@@ -27,3 +46,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+    
